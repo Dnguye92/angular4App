0bb3ae27b4373a6772e873b0f5db971a4ed2cefa
Dylan Nguyen <Dnguye92@yahoo.com>
2017-05-06 00:07:08 -0700

    Taking server data from input w/ 2-way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -23,4 +24,9 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'A server was created!';
   }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
 }
